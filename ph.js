1e23a7b762514e51ac0ab023c8519e3a20c98f8c
ESTERANJANI <105189188+ESTERANJANI@users.noreply.github.com>
2022-05-09 11:57:18 +0700

    PH

---
+++ ph.js
@@ -0,0 +1,100 @@
+let gedget = []; 
+
+let phone = {
+nama: "iPhone 13 pro max",
+harga:"2200000",
+reting:"5.0",
+stog:"10",
+foto: "https://cdn.eraspace.com/pub/media/catalog/product/i/p/iphone_13_pro_max_silver_1.jpg"
+}
+
+gedget.push(phone)
+
+phone= {
+    nama:"samsung A7 Lite",
+    harga:"50000000",
+    reting:"5.0",
+    stog:"20",
+    Kode:"BRG002",
+    foto:"https://asset.kompas.com/crops/pF7fpgD9EWkLF8zjpQpxY0tDJBg=/57x148:595x507/750x500/data/photo/2021/05/28/60b030701f6de.png"
+}
+gedget.push(phone)
+
+phone = {
+    nama:"OPPO RENO 9",
+    reting:"5.0",
+    stog:"100",
+    harga:"700000",
+    foto: "https://jasystore.com/wp-content/uploads/2021/12/oppo-reno8-5g-1-500x500.jpg"
+}
+
+gedget.push(phone)
+
+phone = {
+    nama:"Xiaomi redmi note 11",
+    harga:5000000,
+    reting:"5.0",
+    kode : "BRG004",
+    stog:"1000",
+    foto: "https://i01.appmifile.com/webfile/globalimg/id/cms/10B4246A-5AB3-13F7-B7F7-082776A7B5E7.jpg"
+}
+
+gedget.push(phone)
+
+phone = {
+    nama:"VIVO",
+    harga:"60000000",
+    reting:"5.0",
+    stog:"50",
+    foto: "http://p-id.ipricegroup.com/uploaded_70ab37a1b36321e32535fe8d44c4259a.jpg"
+}
+
+gedget.push(phone)
+
+phone = {
+    nama:"redmi note 11",
+    harga:"70000000",
+    reting:"5.0",
+    stog:"500",
+    foto: "https://i01.appmifile.com/webfile/globalimg/id/cms/87BE5C0A-B625-81D6-00A1-D62E7CCEF8D8.jpg"
+}
+gedget.push(phone)
+
+phone = {
+    nama:"oppo a5",
+    harga:"5000000",
+    reting:"5.0",
+    stog:"70",
+    foto: "http://p-id.ipricegroup.com/uploaded_6032deeb8d6e2e0f09b0be46d1afc245.jpg"
+}
+    gedget.push(phone)
+
+phone = {
+        nama:"oppo a57",
+        harga:"700000",
+        reting:"5.0",
+        stog:"60",
+        foto: "https://cf.shopee.co.id/file/3b113facf3174b61a5670b87546d9161"
+    }
+      gedget.push(phone)
+
+phone = {
+        nama:"oppo a3s",
+        harga:"600000",
+        reting:"5.0",
+        stog:"800",
+        foto: "https://fdn2.gsmarena.com/vv/bigpic/oppo-a3s.jpg"
+        
+      }
+      gedget.push(phone)
+      
+phone = {
+        nama:"redmi 9t",
+        harga:"800000",
+        reting:"5.0",
+        stog:"800",
+        foto: "https://i01.appmifile.com/webfile/globalimg/id/cms/5EB4E1CE-57DD-FF23-E28D-CEE5D5590C37.jpg"
+      }
+      gedget.push(phone)
+console.log(gedget);
+
